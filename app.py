cf81597598ee7fdadf3dd508167901966955b4bc
wjx <4838@vetech.cn>
2026-09-20 09:38:47 +0800

    修复bug

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def serve():
 def home():
     return render_template('home.html',
         is_subpage=False,
-        active_nav='Project',
         current_path='/')
 
 @app.route('/project/description')
